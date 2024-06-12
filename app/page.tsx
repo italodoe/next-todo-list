ef838cec996bf1e57eb24305d60b58e0f84137af
Italo Castillo <italogitdoe@gmail.com>
2024-06-12 22:13:33 +0200

    add form

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex flex-col flex-nowrap items-center pt-11">
       <h1>   TODO LIST </h1>
+      <form className="p-3 ">
+        <input type="text" name="What!" className="border border-black p-1 mr-2 rounded"></input>
+        <button>Add</button>
+      </form>
     </main>
   );
 }
