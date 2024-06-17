f339c2cb565b7baac617a073404a008bf59e371c
Italo Castillo <italogitdoe@gmail.com>
2024-06-17 22:54:08 +0200

    add turso db

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
+import { dbGetTodos } from "@/lib/todos";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const todos = await dbGetTodos();
   return (
     <main className="flex flex-col flex-nowrap items-center pt-11">
-      <h1>   TODO LIST </h1>
+      <h1> TODO LIST </h1>
       <form className="p-3 ">
-        <input type="text" name="What!" className="border border-black p-1 mr-2 rounded"></input>
+        <input
+          type="text"
+          name="What"
+          className="border border-black p-1 mr-2 rounded"
+        ></input>
         <button>Add</button>
       </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}> {todo.what}</li>
+        ))}
+      </ul>
     </main>
   );
 }
